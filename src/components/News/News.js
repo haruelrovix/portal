358a8d0168fbba8087e8c8f7929963f0702e8a8d
Havit Choirul Rovik <Havit_R@mitrais.com>
2018-04-23 13:13:15 +0700

    Fetch data and update your state

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import NewSingle from './NewSingle';
 
 class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ad2a6f3e435648c4bf8c7c110bae6a1e';
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles
+        })
+      })
+      .catch((error) => console.log(error));
+  }
+
   renderItems() {
-    return this.props.items.map((item) => (
+    return this.state.news.map((item) => (
       <NewSingle key={item.id} item={item} />
     ));
   }
