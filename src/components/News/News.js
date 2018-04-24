b8663c8c40891594398fa18bec3a0ad6f3d9244c
Havit Choirul Rovik <Havit_R@mitrais.com>
2018-04-24 09:29:50 +0700

    Add an error component for fetch

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NewSingle from './NewSingle';
+import Error from './Error';
 
 class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
       news: [],
+      error: false
     };
   }
 
@@ -21,13 +23,19 @@ class News extends Component {
           news: data.articles
         })
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        this.setState({
+          error: true
+        });
+      });
   }
 
   renderItems() {
-    return this.state.news.map((item) => (
-      <NewSingle key={item.url} item={item} />
-    ));
+    return !this.state.error ?
+      this.state.news.map((item) => (
+        <NewSingle key={item.url} item={item} />
+      ))
+      : <Error />;
   }
 
   render() {
