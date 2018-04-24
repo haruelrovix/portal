956d620b49181535f159685376e76bc048be7e02
Havit Choirul Rovik <Havit_R@mitrais.com>
2018-04-24 09:12:53 +0700

    Add new methods for axios

--- src/components/News/Sidenews.js
+++ src/components/News/Sidenews.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SingleSide from './SingleSide';
+import axios from 'axios';
 
 class Sidenews extends Component {
   constructor(props) {
@@ -10,7 +11,15 @@ class Sidenews extends Component {
   }
 
   componentDidMount() {
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=ad2a6f3e435648c4bf8c7c110bae6a1e`;
 
+    axios.get(url)
+      .then((response) => {
+        this.setState({
+          sidenews: response.data.articles
+        });
+      })
+      .catch((error) => console.log(error));
   }
 
   renderItems() {
